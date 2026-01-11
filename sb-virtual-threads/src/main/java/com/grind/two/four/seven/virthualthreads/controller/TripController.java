547e2c77130bc4c76cf4d497034241c6152a64d6
Celestino Agudo <celestinoagudo07@gmail.com>
2026-01-12 03:21:55 +0800

    logs for testing

--- sb-virtual-threads/src/main/java/com/grind/two/four/seven/virthualthreads/controller/TripController.java
+++ sb-virtual-threads/src/main/java/com/grind/two/four/seven/virthualthreads/controller/TripController.java
@@ -6,6 +6,8 @@ import com.grind.two.four.seven.virthualthreads.dto.TripReservationRequest;
 import com.grind.two.four.seven.virthualthreads.service.TripPlanService;
 import com.grind.two.four.seven.virthualthreads.service.TripReservationService;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -13,11 +15,14 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class TripController {
 
+    Logger logger = LoggerFactory.getLogger(TripController.class);
+
     private final TripPlanService planService;
     private final TripReservationService reservationService;
 
     @GetMapping("{airportCode}")
     public TripPlan planTrip(@PathVariable final String airportCode) {
+        logger.info("Is Virtual? {}", Thread.currentThread().isVirtual());
         return planService.getTripPlan(airportCode);
     }
 
