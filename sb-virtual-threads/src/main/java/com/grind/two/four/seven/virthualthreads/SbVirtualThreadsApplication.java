1f7d4c0216f5586a659f9748fe8a6d3f457b7a0e
Celestino Agudo <celestinoagudo07@gmail.com>
2026-01-11 22:42:02 +0800

    initial files

---
+++ sb-virtual-threads/src/main/java/com/grind/two/four/seven/virthualthreads/SbVirtualThreadsApplication.java
@@ -0,0 +1,13 @@
+package com.grind.two.four.seven.virthualthreads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbVirtualThreadsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbVirtualThreadsApplication.class, args);
+    }
+
+}
---
+++ sb-virtual-threads/src/test/java/com/grind/two/four/seven/virthualthreads/VirthualthreadsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grind.two.four.seven.virthualthreads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirthualthreadsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
